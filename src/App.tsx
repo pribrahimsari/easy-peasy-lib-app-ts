d23e1c540f535b4660c8737e9541f1fb04ff48e8
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-19 00:42:11 +0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return <div className="App">
+    <h1>Hello World</h1>
+    <h2>This cours is bla bla bla</h2>
+  </div>;
+}
+
+export default App;
