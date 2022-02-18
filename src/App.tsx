9887d691f2aab24fcb82f729ad27fed9b52f1926
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-19 01:58:00 +0300

    first action to setname

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import {useStoreState} from "./store/hooks";
+import React, {useState} from 'react';
+import {useStoreActions, useStoreState} from "./store/hooks";
 
 
 function App() {
-  const { name, course }= useStoreState((store) => store);
+  const { name, course } = useStoreState((store) => store);
+  const { setName } = useStoreActions(store =>store);
+  const [inputName, setInputName] = useState("");
+
   return <div className="App">
     <h1>Hello {name}</h1>
     <h2>This course {course}</h2>
+    <h3>{inputName}</h3>
+    <input type="text" placeholder={name} value={inputName} onChange={(e)=>setInputName(e.target.value)} />
+    <button onClick={()=>setName(inputName)}>Submit</button>
   </div>;
 }
 
--- src/store/models/index.ts
+++ src/store/models/index.ts
@@ -1,11 +1,17 @@
+import {action, Action} from "easy-peasy";
+
 export interface StoreModel {
     name:string;
     course:string;
+    setName: Action<this, string>;
 }
 
 const model:StoreModel = {
     name: "İbrahim",
     course: "Easy Peasy TypeScript React Course",
+    setName: action((state, payload)=>{
+        state.name = payload;
+    }),
 };
 
 export default model;
