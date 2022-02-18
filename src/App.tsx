525dea568c1a6f053f1dc3e8b5f9fb00a0d915d2
pribrahimsari <pr.ibrahimsari@gmail.com>
2022-02-19 02:24:58 +0300

    introduction for easy peasy thunks

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,27 @@ import {useStoreActions, useStoreState} from "./store/hooks";
 
 function App() {
   const { name, course } = useStoreState((store) => store);
-  const { setName } = useStoreActions(store =>store);
+  const { setName, updateDataThunk } = useStoreActions(store =>store);
+
   const [inputName, setInputName] = useState("");
+  const [inputCourse, setInputCourse] = useState("");
+
+  const handleSubmitBtn = () => {
+    updateDataThunk({
+      name: inputName,
+      course: inputCourse,
+    });
+  }
 
   return <div className="App">
     <h1>Hello {name}</h1>
     <h2>This course {course}</h2>
-    <h3>{inputName}</h3>
-    <input type="text" placeholder={name} value={inputName} onChange={(e)=>setInputName(e.target.value)} />
-    <button onClick={()=>setName(inputName)}>Submit</button>
+    <h4>{inputName}</h4>
+    <h4>{inputCourse}</h4>
+
+    <input type="text" placeholder={name} value={inputName} onChange={(e)=>setInputName(e.target.value)} /> <br />
+    <input type="text" placeholder={course} value={inputCourse} onChange={(e)=>setInputCourse(e.target.value)} /> <br />
+    <button onClick={handleSubmitBtn}>Submit</button>
   </div>;
 }
 
--- src/store/models/index.ts
+++ src/store/models/index.ts
@@ -1,17 +1,39 @@
-import {action, Action} from "easy-peasy";
+import {action, Action, thunk, Thunk} from "easy-peasy";
 
-export interface StoreModel {
-    name:string;
-    course:string;
-    setName: Action<this, string>;
+interface UpdateDataPayload {
+    name: string;
+    course: string;
 }
 
-const model:StoreModel = {
+export interface StoreModelState {
+    name:string;
+    course:string;
+}
+
+export interface StoreModelActions {
+    setName: Action<this, string>;
+    setCourse: Action<this, string>;
+}
+
+export interface StoreModelThunks {
+    updateDataThunk: Thunk<this, UpdateDataPayload>
+}
+
+export interface StoreModel extends StoreModelState, StoreModelActions, StoreModelThunks {};
+
+const model: StoreModel = {
     name: "İbrahim",
     course: "Easy Peasy TypeScript React Course",
     setName: action((state, payload)=>{
         state.name = payload;
     }),
+    setCourse: action((state, payload) => {
+        state.course = payload;
+    }),
+    updateDataThunk: thunk(async (actions, payload) => {
+        actions.setName(payload.name);
+        actions.setCourse(payload.course);
+    })
 };
 
 export default model;
